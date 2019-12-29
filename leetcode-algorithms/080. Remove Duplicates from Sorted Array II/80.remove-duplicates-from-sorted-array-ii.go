7f8475e80a4c592304c1af714d535ce798b0e96e
杨开南 <yangkainan@bytedance.com>
2019-12-29 16:23:27 +0800

    080. Remove Duplicates from Sorted Array II
    Change-Id: I55978ad9cac4615cfe697ba62fbb50165a3b23ca

--- leetcode-algorithms/080. Remove Duplicates from Sorted Array II/80.remove-duplicates-from-sorted-array-ii.go
+++ leetcode-algorithms/080. Remove Duplicates from Sorted Array II/80.remove-duplicates-from-sorted-array-ii.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 	"github.com/google/go-cmp/cmp"
 )
+/*
+
+心得:
+1. 界定 开启一轮 移动的时机: 发现了 Duplicate 串, 且已经到达的 Duplicate 串的"尾"
+2. 在 移动的时候, 要明确移动的范围:  新的起点, 原起点, 移动多少个元素.
+	移动多少个元素可以放松到, 每次都移动到 Nums 的尾.
+3. 确定 算法的结束条件(确保一定结束) (金字塔原则: 不重, 不漏): "遍历了整个 Nums 数组". 但是,
+	因为, Nums 的内容会不停的"向前"移动, 所以, "当前位置"在移动后,也要同步的"前移". (否则会 "漏"元素)
+	同时, 因为, 移动后,"尾部"元素相当于已经删除了, 一定不要访问(因为, 删除部分也可能包含 Duplicate 串, 会重复计算).(否则会 "重复")
+
+4. 还有要注意, "循环体结束后", 会不会出现 "遗留"的, 没有被计算的 Duplicate 串(只时, 已经不需要 移动了).
+
+在写的过程中, 最开始没有识别出 "RemovedLen" 这个变量, 它的核心是标识"尾部被删除的范围", 同时也可以用来 计算 "新长度".
+之后, RemoveLen 的计算是基于"DupEnd 和 DupStart"来做的, 但是, 没有想明白 "当这两个 Pos 相同的时候,要怎么计算" (DupEnd-DupStart 后要不要+1. 加与不加都不对... )
+细致的分析一下, 发现, 应该直接使用 DupLen 来计算.  DupEnd,DupStart 只是在移动的时候使用.
+
+*/
 
 /*
 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
@@ -26,8 +43,10 @@ import (
 
 你不需要考虑数组中超出新长度后面的元素。
 */
+
+const DefinitionOfDuplicate = 2
 func removeDuplicates(nums []int) int {
-	if len(nums) <= 2 {
+	if len(nums) <= DefinitionOfDuplicate {
 		return len(nums)
 	}
 
@@ -35,31 +54,31 @@ func removeDuplicates(nums []int) int {
 
 	curPos := 1
 
-	startPos, endPos := curPos, curPos
+	dupStart, dupEnd := curPos, curPos
 
 	dupLen := 1
 	for ; curPos < len(nums) - removedLen; curPos++ {
 
 		if nums[curPos- 1] == nums[curPos] {
 			dupLen ++
-			if dupLen == 3 {
-				startPos = curPos
+			if dupLen == (DefinitionOfDuplicate + 1) {
+				dupStart = curPos
 			}
 		} else {
-			endPos = curPos
-			if dupLen > 2 {
-				numsOfElemToMove := len(nums) - removedLen - endPos
-				move(nums, startPos, endPos, numsOfElemToMove)
-				removedLen += dupLen - 2
-				curPos = startPos
+			dupEnd = curPos
+			if dupLen > DefinitionOfDuplicate {
+				numsOfElemToMove := len(nums) - removedLen - dupEnd
+				move(nums, dupStart, dupEnd, numsOfElemToMove)
+				removedLen += dupLen - DefinitionOfDuplicate
+				curPos = dupStart
 			}
 			dupLen = 1
 		}
 	}
 
 	// 最后一个连续串, 长度大于3
-	if dupLen > 2 {
-		removedLen += dupLen - 2
+	if dupLen > DefinitionOfDuplicate {
+		removedLen += dupLen - DefinitionOfDuplicate
 	}
 
 	return len(nums) - removedLen
